a5605266b676f3a54a58bdda02fe622cb392f0a4
Efimenko VE <efimenko.ve@nsd.ru>
2025-11-23 14:18:42 +0300

    Добавлены пакеты tailwind и tailwindcss

---
+++ app/layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="layout">
+    <header class="layout__header">
+      <div class="container flex">sdf</div>
+    </header>
+
+    <slot />
+
+    <footer class="layout__footer"></footer>
+  </div>
+</template>
+
+<style scoped></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  css: ['./app/assets/css/main.css'],
 })
